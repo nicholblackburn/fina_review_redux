db536047887184616d449a2fca1dea48a5453b9b
Nichol Blackburn <nicholblackburn@outlook.com>
2018-10-22 12:27:36 -0600

    added react-redux

---
+++ src/components/Ledger.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import LedgerForm from './LedgerForm'
+import Transactions from './Transactions'
+
+const Ledger = () => (
+  <div>
+    <LedgerForm />
+    <Transactions />
+  </div>
+)
+
+export default Ledger 
+
---
+++ src/components/WishList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import WishListForm from './WishListForm'
+import Items from './Items'
+
+const WishList = () => (
+  <div>
+    <WishListForm />
+    <Items />
+  </div>
+)
+
+export default WishList
+
+
